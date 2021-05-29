c2e7724a315cf116907da53c39e9252f93c8d7c9
lol <81094360+shrate@users.noreply.github.com>
2021-05-29 18:03:52 +0200

    Add files via upload

---
+++ shop/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="app/app.js"></script>
+    <link rel="stylesheet" href="css/index.css">
+    <title>Shop</title>
+</head>
+
+<body><form action="profile.php" method="GET" enc>
+    <div class="login flex">
+        <h2>Log in</h2>
+        <p><input type="text" name="username" id="username" placeholder="username"></p>
+        <p><input type="password" name="password" id="password" placeholder="password"></p>
+        <p><input type="submit" value="submit" id="submit" name="submit">
+        
+    </div>
+    </form>
+
+    <div class="register flex">
+        <h2>Register</h2>
+        <p><input type="text" name="username" id="register-username"></p>
+        <p><input type="password" name="password" id="register-password"></p>
+        <p><input type="submit" value="submit" id="submit" name="submit">
+    </div>
+
+</body>
+
+
+
+</html>
